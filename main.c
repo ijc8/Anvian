6ce124f0e51a5aaf5a7931c737c19d349d8a8fbe
bgs100 <ijcguy1@gmail.com>
2011-12-24 22:18:25 -0500

    Now there are *gasp* 3 dimensions! Currently 3 / 4 sides display...

--- main.c
+++ main.c
@@ -4,47 +4,115 @@
 #include "util.h"
 
 GLuint program;
-GLuint attrCoord, attrTexcoord;
-GLuint texture, uniformTexture;
+GLuint coordAttr, texcoordAttr;
+GLuint texture, textureUniform;
 GLuint vbo, vboTexcoords;
 
+GLuint zNearUniform;
+GLuint zFarUniform;
+GLuint frustumScaleUniform;
+
 GLfloat vertices[] = {
-    0.5,  0.5,
-    0.5, -0.5,
-   -0.5, -0.5,
-   -0.5,  0.5,
+    /* front */
+   -0.5,  0.5, 0.5,
+    0.5,  0.5, 0.5,
+    0.5, -0.5, 0.5,
+   -0.5, -0.5, 0.5,
+
+   /* left */
+  -0.5,  0.5, -0.5,
+  -0.5,  0.5,  0.5,
+  -0.5, -0.5,  0.5,
+  -0.5, -0.5, -0.5,
+    
+   /* right */
+   0.5,  0.5,  0.5,
+   0.5,  0.5, -0.5,
+   0.5, -0.5, -0.5,
+   0.5, -0.5,  0.5,
+
+   /* back */
+   0.5,  0.5, -0.5,
+  -0.5,  0.5, -0.5,
+  -0.5, -0.5, -0.5,
+   0.5, -0.5, -0.5,   
 };
 
 GLfloat texcoords[] = {
     /* for each y, 1 - y was done to account for the bottom-left thing */
+    0.0, 0.0,
     1.0, 0.0,
     1.0, 1.0,
     0.0, 1.0,
+
+    /* repeat */
     0.0, 0.0,
+    1.0, 0.0,
+    1.0, 1.0,
+    0.0, 1.0,
+
+    0.0, 0.0,
+    1.0, 0.0,
+    1.0, 1.0,
+    0.0, 1.0,
+
+    0.0, 0.0,
+    1.0, 0.0,
+    1.0, 1.0,
+    0.0, 1.0,
 };
 
 int initGL() {
+    glEnable(GL_DEPTH_TEST);
+    glDepthMask(GL_TRUE);
+    glDepthFunc(GL_LEQUAL);
+    glDepthRange(0.0, 1.0);
+
     GLuint vertexShader = loadShader(GL_VERTEX_SHADER, "standard.vert");
     GLuint fragShader = loadShader(GL_FRAGMENT_SHADER, "standard.frag");
     program = createProgram(vertexShader, fragShader);
 
-    attrCoord = glGetAttribLocation(program, "coord");
-    if (attrCoord == -1) {
+    coordAttr = glGetAttribLocation(program, "coord");
+    if (coordAttr == -1) {
         fprintf(stderr, "Could not bind attribute \"coord\"\n");
         return 0;
     }
 
-    attrTexcoord = glGetAttribLocation(program, "texcoord");
-    if (attrTexcoord == -1) {
+    texcoordAttr = glGetAttribLocation(program, "texcoord");
+    if (texcoordAttr == -1) {
         fprintf(stderr, "Could not bind attribute \"texcoord\"\n");
         return 0;
     }
 
-    uniformTexture = glGetUniformLocation(program, "mytexture");
-    if (uniformTexture == -1) {
+    textureUniform = glGetUniformLocation(program, "mytexture");
+    if (textureUniform == -1) {
         fprintf(stderr, "Could not bind uniform \"mytexture\"\n");
         return 0;
     }
+
+    zNearUniform = glGetUniformLocation(program, "zNear");
+    if (zNearUniform == -1) {
+        fprintf(stderr, "Could not bind uniform \"zNear\"\n");
+        return 0;
+    }
+
+    zFarUniform = glGetUniformLocation(program, "zFar");
+    if (zFarUniform == -1) {
+        fprintf(stderr, "Could not bind uniform \"zFar\"\n");
+        return 0;
+    }
+
+    frustumScaleUniform = glGetUniformLocation(program, "frustumScale");
+    if (frustumScaleUniform == -1) {
+        fprintf(stderr, "Could not bind uniform \"frustumScale\"\n");
+        return 0;
+    }
+
+    glUseProgram(program);
+    glUniform1f(frustumScaleUniform, 1.0f);
+    glUniform1f(zNearUniform, 1.0f);
+    glUniform1f(zFarUniform, 3.0f);
+    glUseProgram(0);
 
     glGenBuffers(1, &vbo);
     glBindBuffer(GL_ARRAY_BUFFER, vbo);
@@ -63,21 +131,22 @@ int initGL() {
 
 void display() {
     glClearColor(0.53, 0.8, 0.98, 1.0);
-    glClear(GL_COLOR_BUFFER_BIT);
+    glClearDepth(1.0);
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
     glUseProgram(program);
-    glEnableVertexAttribArray(attrCoord);
+    glEnableVertexAttribArray(coordAttr);
     glBindBuffer(GL_ARRAY_BUFFER, vbo);
-    glVertexAttribPointer(attrCoord, 2, GL_FLOAT, GL_FALSE, 0, 0);
+    glVertexAttribPointer(coordAttr, 3, GL_FLOAT, GL_FALSE, 0, 0);
 
-    glUniform1i(uniformTexture, 0);
-    glEnableVertexAttribArray(attrTexcoord);
+    glUniform1i(textureUniform, 0);
+    glEnableVertexAttribArray(texcoordAttr);
     glBindBuffer(GL_ARRAY_BUFFER, vboTexcoords);
-    glVertexAttribPointer(attrTexcoord, 2, GL_FLOAT, GL_FALSE, 0, 0);
+    glVertexAttribPointer(texcoordAttr, 2, GL_FLOAT, GL_FALSE, 0, 0);
 
-    glDrawArrays(GL_QUADS, 0, 4);
-    glDisableVertexAttribArray(attrCoord);
-    glDisableVertexAttribArray(attrTexcoord);
+    glDrawArrays(GL_QUADS, 0, 16);
+    glDisableVertexAttribArray(coordAttr);
+    glDisableVertexAttribArray(texcoordAttr);
     glBindBuffer(GL_ARRAY_BUFFER, 0);
 
     glutSwapBuffers();
--- standard.vert
+++ standard.vert
@@ -1,10 +1,24 @@
 #version 120
 
-attribute vec2 coord;
+attribute vec3 coord;
 attribute vec2 texcoord;
 varying vec2 f_texcoord;
 
+uniform float zNear;
+uniform float zFar;
+uniform float frustumScale;
+
 void main() {
-    gl_Position = vec4(coord, 0.0, 1.0);
+    vec4 offset = vec4(-1.5, -1.5, -3.0, 0.0);
+    vec4 cameraPos = vec4(coord, 1.0) + offset;
+    vec4 clipPos;
+
+    clipPos.xy = cameraPos.xy * frustumScale;
+    clipPos.z = cameraPos.z * (zNear + zFar) / (zNear - zFar);
+    clipPos.z += 2 * zNear * zFar / (zNear - zFar);
+    clipPos.w = -cameraPos.z;
+
+    gl_Position = clipPos;
+    
     f_texcoord = texcoord;
 }
