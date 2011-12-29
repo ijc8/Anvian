f36b463aef668b20bd5de48a47dd0c760ff26b47
bgs100 <ijcguy1@gmail.com>
2011-12-28 20:15:00 -0500

    Cleaned up matrix code with a macro.

--- matrix.c
+++ matrix.c
@@ -3,6 +3,8 @@
 #include <stdio.h>
 #include "matrix.h"
 
+#define I(mat, row, column) mat.data[row * mat.columns + column]
+
 matrix makeMatrix(float *data, int rows, int columns) {
     matrix mat;
     mat.data = data;
@@ -21,43 +23,45 @@ matrix identity(int size) {
     matrix result = emptyMatrix(size, size);
     int i;
     for (i = 0; i < size; i++)
-        result.data[i*size+i] = 1;
+        I(result, i, i) = 1;
     return result;
 }
 
 matrix multiply(matrix a, matrix b) {
     if (a.columns != b.rows)
         return (matrix){0};
-    float *data = malloc(a.rows * b.columns * sizeof(float));
+    matrix result = makeMatrix(malloc(a.rows * b.columns * sizeof(float)),
+                               a.rows, b.columns);
     int r, c;
     for (r = 0; r < a.rows; r++) {
         for (c = 0; c < b.columns; c++) {
             float sum;
             int i;
             for (sum = i = 0; i < a.columns; i++)
-                sum += a.data[r*a.columns+i] * b.data[i*b.columns+c];
-            data[r*b.columns+c] = sum;
+                sum += I(a, r, i) * I(b, i, c);
+            I(result, r, c) = sum;            
         }
     }
-    return makeMatrix(data, a.rows, b.columns);          
+    return result;
 }
 
 matrix translate(float x, float y, float z) {
     matrix result = identity(4);
-    result.data[4*0+3] = x;
-    result.data[4*1+3] = y;
-    result.data[4*2+3] = z;
+
+    I(result, 0, 3) = x;
+    I(result, 1, 3) = y;
+    I(result, 2, 3) = z;
     return result;
 }
 
 matrix perspective(float frustumScale, float zNear, float zFar) {
     matrix result = emptyMatrix(4, 4);
 
-    result.data[4*0+0] = frustumScale;
-    result.data[4*1+1] = frustumScale;
-    result.data[4*2+2] = (zFar + zNear) / (zNear - zFar);
-    result.data[4*2+3] = (2 * zFar * zNear) / (zNear - zFar);
-    result.data[4*3+2] = -1;
+    I(result, 0, 0) = frustumScale;
+    I(result, 1, 1) = frustumScale;
+    I(result, 2, 2) = (zFar + zNear) / (zNear - zFar);
+    I(result, 2, 3) = (2 * zFar * zNear) / (zNear - zFar);
+    I(result, 3, 2) = -1;
 
     return result;
 }
@@ -66,13 +70,12 @@ void printMatrix(matrix m) {
     int r, c;
     printf("[ ");
     for (c = 0; c < m.columns; c++)
-            printf("%g ", m.data[c]);
+        printf("%g ", I(m, 0, c));
     for (r = 1; r < m.rows; r++) {
         putchar('\n');
         printf("  ");
-        for (c = 0; c < m.columns; c++) {
-            printf("%g ", m.data[r*m.columns+c]);
-        }
+        for (c = 0; c < m.columns; c++)
+            printf("%g ", I(m, r, c));
     }
     printf("]\n");
 }
